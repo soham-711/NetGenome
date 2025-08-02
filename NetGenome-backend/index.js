c7466870bcbbec372feda0f245cc3723f1448a1c
Soham-711 <sohambiswas711@gmail.com>
2025-08-02 20:44:21 +0530

    All done

--- NetGenome-backend/index.js
+++ NetGenome-backend/index.js
@@ -26,6 +26,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174", // ✅ Add this
       "https://net-genome.vercel.app",
+      "https://net-genome-artist.vercel.app/",
     ],
     credentials: true,
   })
